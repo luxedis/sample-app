08d45a2d5c535582145c0d9422cc238254cac701
luxedis <08ainams@gmail.com>
2020-09-09 02:18:09 +0000

    ログイン後のヘッダー編集完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  # 現ログインユーザーが管理者かどうか
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+  
 end
