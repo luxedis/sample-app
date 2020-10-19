3f5c800fcd506a10cd6e3bf0ec1e9e953fb77a4d
luxedis <08ainams@gmail.com>
2020-10-19 03:03:20 +0000

    rails4.6destroyアクション途中

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get 'tasks/index', to: 'tasks#show'
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  
+  post 'tasks/:id/destroy', to: 'tasks#destroy'
   
   resources :users do
     resources :tasks
