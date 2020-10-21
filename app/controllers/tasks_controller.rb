2533c979bf05d8bcac8a3b6ca3e2a5364009d71f
luxedis <08ainams@gmail.com>
2020-10-21 06:42:28 +0000

    C8実装完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
   
     def task_params
-      params.permit(:name, :description)
+      params.require(:task).permit(:name, :description)
     end
     
     def set_user
