10d379abc3d801134dc4e5421be4a6cb5020bae0
luxedis <08ainams@gmail.com>
2020-11-25 05:52:15 +0000

    タスク降順実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_user
   
   def index
-    @tasks = @user.tasks.order(created_at: :asc)
+    @tasks = @user.tasks.order(created_at: :desc)
   end
   
   def show
